ba43e45873b131a11bbbf59ec12970ca2112fca0
badireddisatyasaivarun <41924086+badireddisatyasaivarun@users.noreply.github.com>
2021-06-28 15:53:15 +0530

    all args constructor

--- Booking/src/main/java/com/Booking/Booking/Model/BookingPostRequest.java
+++ Booking/src/main/java/com/Booking/Booking/Model/BookingPostRequest.java
@@ -1,8 +1,9 @@
 package com.Booking.Booking.Model;
 
 import java.util.List;
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
