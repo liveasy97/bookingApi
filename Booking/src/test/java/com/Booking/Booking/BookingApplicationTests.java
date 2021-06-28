660f516b81ecf0edf32f660af5de36857ec65d48
badireddisatyasaivarun <41924086+badireddisatyasaivarun@users.noreply.github.com>
2021-06-28 15:56:07 +0530

    sample test

--- Booking/src/test/java/com/Booking/Booking/BookingApplicationTests.java
+++ Booking/src/test/java/com/Booking/Booking/BookingApplicationTests.java
@@ -1,13 +1,34 @@
 package com.Booking.Booking;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.Booking.Booking.Dao.BookingDao;
+import com.Booking.Booking.Entities.BookingData;
+import com.Booking.Booking.Model.BookingPostRequest;
+import com.Booking.Booking.Model.BookingPostResponse;
+import com.Booking.Booking.Service.BookingServiceImpl;
 
 @SpringBootTest
 class BookingApplicationTests {
 
+	@Autowired
+	BookingServiceImpl service;
+	
+	@MockBean
+	BookingDao bookingDao;
+	
 	@Test
-	void sample() {
+	void simple() {
 		List<String> truck =new ArrayList<String>();
 		truck.add("tr");
 		BookingPostRequest request = new BookingPostRequest("1","2","3",2L,"PER_TON",truck,null);
