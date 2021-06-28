8d6d2254f7728a3799af621211e1e5ee4cce5bbb
badireddisatyasaivarun <41924086+badireddisatyasaivarun@users.noreply.github.com>
2021-06-28 15:41:34 +0530

    Update BookingApplicationTests.java

--- Booking/src/test/java/com/Booking/Booking/BookingApplicationTests.java
+++ Booking/src/test/java/com/Booking/Booking/BookingApplicationTests.java
@@ -7,7 +7,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 class BookingApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void sample() {
+		List<String> truck =new ArrayList<String>();
+		truck.add("tr");
+		BookingPostRequest request = new BookingPostRequest("1","2","3",2L,"PER_TON",truck,null);
+		BookingData data = new BookingData();
+		BookingPostResponse response = new BookingPostResponse();
+		
+		when(bookingDao.save(data)).thenReturn(data);
+		
+		assertEquals("Success",service.addBooking(request).getStatus());
 	}
 
 }
